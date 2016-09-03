e95cdff81a4a152ffa6930682c1cf6c528d25364
Vishwa Shanmugam <18vshanmugam.stem@gmail.com>
2016-09-03 11:01:30 -0400

    Even better formatted Caesar Shifts, now works with special symbols. Added rsahelp command, currently a placeholder. Base functionality of Caesar Shifts complete.

--- CaesarCipher.py
+++ CaesarCipher.py
@@ -1,12 +1,14 @@
 def solveCaesarCipher(text):
+    print("\n")
+    print("\tRunning Caesar Cipher...")
+    print("\tAll possible solution strings : \n")
     for i in range (1, 26):
         newStr = ""
         for l in text:
-            if l == " ":
-                newStr += l
+            if ord("a") <= ord(l) <= ord("z"): #lower case
+                newStr += chr(ord(l) + i) if ord(l) + i <= ord("z") else chr(ord(l)+i-26)
+            elif ord("A") <= ord(l) <= ord("Z"):
+                newStr += chr(ord(l) + i) if ord(l) + i <= ord("Z") else chr(ord(l)+i-26)
             else:
-                if ord(l) > ord("Z"): #lower case
-                    newStr += chr(ord(l) + i) if ord(l) + i <= ord("z") else chr(ord(l)+i-26)
-                else:
-                    newStr += chr(ord(l) + i) if ord(l) + i <= ord("Z") else chr(ord(l)+i-26)
-        print(newStr + "\n")
+                newStr += l
+        print("\t" + newStr + "\n")
--- CipherSolver.py
+++ CipherSolver.py
@@ -9,8 +9,7 @@ elif len(sys.argv) == 2:
         print("""
         Welcome to Super Cipher Solver! With this utility you can solve some types of ciphers.
         We currently support base64 decoding, caesar ciphers, and certain RSA decryption techniques. \n
-        Run this program with the command scs %text, where %text is the text you want to decrypt. Additionally, you
-        may use these flags :
+        Run this program by inputting your text, as well as any number of flags from below:
 
         -f or -fi or -file  :  Read input from a text file rather than inputting it into the command line \n
         -a or -all or -alltests : Run the full suite of cipher tests. Useful if you don't know what cipher you're dealing
@@ -19,33 +18,37 @@ elif len(sys.argv) == 2:
         -r or -rsa : Run certain RSA attacks against the input. See more help with -rhelp or -rsahelp\n
         -b or -b64 or -base64 : Decode the string as base64 only
 
+        IMPORTANT : Multi-word text should be surrounded by quotes as such "I have too many words"
+
         If you notice any bugs or have any suggestions, email us at scsdevteam@gmail.com
         """)
-    else:
+    elif sys.argv[1] == 'rhelp' or sys.arv[1] == 'rsahelp':
+        print("""
+        RSA is currently not functional. Check back later!""")
+
+    else:  # 1 Arg, text to solve
         if input("Would you like to run the full suite of tests on this input? (Y/N)") == "Y":
             print("Running Full Suite...")
             s.try_solve(sys.argv[1], True, False, False, False)
         else:
             print("""You can run specific methods by using command line flags. Call this program with 'Help' for more
             information""")
-else:
-    print("reading input...")
+
+else:  # > 1 args, some are flags
     file_input = False
     allTests = False
     caesar = False
     RSA = False
     base64 = False
-    #other ciphers here
+    # other ciphers here
 
     for arg in sys.argv:
-        print(arg.lower() == "-c")
         if arg.lower() == "-f" or arg.lower() == "-fi" or arg.lower() == "-file":
             file_input = True
         elif arg.lower() == "-a" or arg.lower() == "-all" or arg.lower() == "-alltests":
             allTests = True
         elif arg.lower() == "-c" or arg.lower() == "-caesar":
             caesar = True
-            print("woo")
         elif arg.lower() == "-r" or arg.lower() == "-rsa":
             RSA = True
         elif arg.lower() == "-b" or arg.lower() == "-b64" or arg.lower() == "-base64":
@@ -56,12 +59,4 @@ else:
                     s.try_solve(file.read(),  allTests, caesar, RSA, base64)
             else:
                 s.try_solve(arg,  allTests, caesar, RSA, base64)
-print(ord("Z"))
-print(ord("z"))
-# elif len(sys.argv) == 3:
-#     if sys.argv[1] == 'file':
-#         with open(sys.argv[2], 'r') as file:
-#             s.trySolve(file.read())
-#             print(file.read)
-# Broken rn
 
